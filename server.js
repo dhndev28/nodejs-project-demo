dd30e8babbfa76cfe5f2e2bced63008020cedf0e
Nam Dao <nd978975@gmail.com>
2024-10-27 14:26:22 +0700

    first commit init

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict'
+
+require('dotenv').config()
+const app = require('./src/app')
+
+const env = process.env
+const PORT = (env.NODE_ENV === 'dev' ? env.APP_DEV_PORT : env.APP_PRO_PORT) || 3059
+
+app.listen(PORT, () => {
+    console.log(`Run app with port: ${PORT}`)
+})
---
+++ src/app.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const express = require('express');
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use((req, res, next) => {
+    return res.json({
+        message: "Xin chao cac ban!"
+    })
+})
+
+app.use((error, req, res, next) => {
+    const statusCode = error.status || 500
+    return res.status(statusCode).json({
+        status: 'error',
+        code: statusCode,
+        stack: error.stack,
+        message: error.message || 'Internal Server Error!'
+    })
+})
+
+module.exports = app
